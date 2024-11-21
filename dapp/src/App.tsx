4f1655e7a614c6ced9ce847c68e13f6a11fdfb51
daseinsucks <alexstepov97@gmail.com>
2024-11-21 16:04:10 +0400

    registration template added

--- dapp/src/App.tsx
+++ dapp/src/App.tsx
@@ -1,27 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import contractABI from './artifacts/LAIRegistrationABI.json';
 
 const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
-
+const APIAddress = process.env.REACT_APP_API_ADDRESS;
+const RegisterEndpoint = process.env.REACT_APP_REGISTER_ENDPOINT;
 if (!contractAddress) {
     throw new Error("Contract address not found in environment variables");
 }
 
 const App: React.FC = () => {
   const [key, setKey] = useState<string>("");
-  const [tgId, setTgId] = useState<number>(0);
+  const [tgId, setTgId] = useState<string>("");
   const [status, setStatus] = useState<string>("");
   const [currentAccount, setCurrentAccount] = useState<string | undefined>();
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const tgIdFromURL = searchParams.get("tgid");
-
-    if (tgIdFromURL) {
-      setTgId(Number(tgIdFromURL));
-    }
-  }, []);
 
   const encryptApiKey = async (apiKey: string): Promise<string> => {
     const publicKeyBase64 = process.env.REACT_APP_PUBLIC_KEY;
@@ -29,7 +20,7 @@ const App: React.FC = () => {
       throw new Error("Public key not found in .env file");
     }
 
-    const publicKeyBuffer = Uint8Array.from(atob(publicKeyBase64), c => c.charCodeAt(0));
+    const publicKeyBuffer = Uint8Array.from(atob(publicKeyBase64), (c) => c.charCodeAt(0));
 
     const publicKey = await window.crypto.subtle.importKey(
       "spki",
@@ -64,6 +55,12 @@ const App: React.FC = () => {
       return;
     }
 
+  const tgIdNumber = Number(tgId)
+    if (tgId && isNaN(Number(tgIdNumber))) {
+      setStatus("Please enter a valid number for Telegram ID.");
+      return;
+    }
+
     try {
       const encryptedKey = await encryptApiKey(key);
 
@@ -73,12 +70,25 @@ const App: React.FC = () => {
         setStatus("No accounts found.");
         return;
       }
-
       const account = accounts[0];
       setCurrentAccount(account);
 
-      //TODO: implement API here
-
+      try {
+        const res = await fetch(`${APIAddress}${RegisterEndpoint}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            wallet: account,
+            apikey: key,
+            telegram: tgId ? tgId : "", // Optional Telegram ID
+          }),
+        });
+        const data = await res.json();
+      } catch (error) {
+        console.error("Error:", error);
+      }
 
       setStatus(`Registration successful! API Key: ${key}`);
     } catch (error) {
@@ -86,6 +96,7 @@ const App: React.FC = () => {
       setStatus("Error occurred.");
     }
   };
+
 
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
@@ -98,7 +109,15 @@ const App: React.FC = () => {
         style={{ padding: "10px", width: "300px", marginBottom: "20px" }}
       />
       <br />
-      <button onClick={handleRegister} disabled={!key}  style={{ padding: "10px 20px" }}>
+      <input
+        type="text"
+        placeholder="Optional Telegram ID"
+        value={tgId}
+        onChange={(e) => setTgId(e.target.value)}
+        style={{ padding: "10px", width: "300px", marginBottom: "20px" }}
+      />
+      <br />
+      <button onClick={handleRegister} disabled={!key} style={{ padding: "10px 20px" }}>
         Register
       </button>
       <p>{status}</p>
--- hardhat.config.ts
+++
@@ -1,8 +0,0 @@
-const { HardhatUserConfig } = require("hardhat/config");
-require("@nomicfoundation/hardhat-toolbox");
-
-const config = {
-  solidity: "0.8.27",
-};
-
-module.exports = config;
